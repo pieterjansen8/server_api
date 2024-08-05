0365e41c5f15a5c5bad41baa127e859d9d845f86
pieter jansen <pieterjansen8@icloud.com>
2024-08-05 23:25:35 +0200

    added a networkcontroller

---
+++ networkcontroller.py
@@ -0,0 +1,10 @@
+import requests
+import time
+
+class network_controller: 
+    url = "https://test-fac11-default-rtdb.europe-west1.firebasedatabase.app/"
+    def push_state():
+        requests.put(json=json, url=)
+
+    
+network_controller.push_state()
