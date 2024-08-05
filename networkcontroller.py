b7ce2d17310586ceb62cc6ae02dcad16eb6fa328
pieter jansen <pieterjansen8@icloud.com>
2024-08-05 23:48:24 +0200

    created starting game option to the network controller

--- networkcontroller.py
+++ networkcontroller.py
@@ -1,10 +1,15 @@
 import requests
 import time
-
+import json
 class network_controller: 
-    url = "https://test-fac11-default-rtdb.europe-west1.firebasedatabase.app/"
-    def push_state():
-        requests.put(json=json, url=)
-
-    
-network_controller.push_state()
+    def create_game():
+        url = "https://test-fac11-default-rtdb.europe-west1.firebasedatabase.app/"
+        r = requests.get(url=url+"game_id.json")
+        j = r.json()
+        id = j["num"]+1
+        body = {
+            "game_num": id
+        }
+        requests.put(json=body, url=url+str(id)+".json")
+        requests.put(json={"num":id}, url=url+"game_id.json")
+        return id
